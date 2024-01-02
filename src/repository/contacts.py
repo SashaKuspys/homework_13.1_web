239d9970cff408c4081fbf1996c5e57b3a9120e9
SashaKuspys <134163412+SashaKuspys@users.noreply.github.com>
2024-01-02 15:43:39 +0200

    HW_13.1

---
+++ src/repository/contacts.py
@@ -0,0 +1,49 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.entity.models import Contact, User
+from src.schemas.contact import ContactSchema, ContactUpdateSchema
+
+
+async def get_contacts(limit: int, offset: int, db: AsyncSession, user: User):
+    stmt = select(Contact).filter_by(user=user).offset(offset).limit(limit)
+    contacts = await db.execute(stmt)
+    return contacts.scalars().all()
+
+
+async def get_contact(contact_id: int, db: AsyncSession, user: User):
+    stmt = select(Contact).filter_by(id=contact_id, user=user)
+    contact = await db.execute(stmt)
+    return contact.scalar_one_or_none()
+
+
+async def create_contact(body: ContactSchema, db: AsyncSession, user: User):
+    contact = Contact(**body.model_dump(exclude_unset=True),
+                      user=user)  # (title=body.title, description=body.description)
+    db.add(contact)
+    await db.commit()
+    await db.refresh(contact)
+    return contact
+
+
+async def update_contact(contact_id: int, body: ContactUpdateSchema, db: AsyncSession, user: User):
+    stmt = select(Contact).filter_by(id=contact_id, user=user)
+    result = await db.execute(stmt)
+    contact = result.scalar_one_or_none()
+    if contact:
+        contact.title = body.title
+        contact.description = body.description
+        contact.completed = body.completed
+        await db.commit()
+        await db.refresh(contact)
+    return contact
+
+
+async def delete_contact(contact_id: int, db: AsyncSession, user: User):
+    stmt = select(Contact).filter_by(id=contact_id, user=user)
+    contact = await db.execute(stmt)
+    contact = contact.scalar_one_or_none()
+    if contact:
+        await db.delete(contact)
+        await db.commit()
+    return contact
